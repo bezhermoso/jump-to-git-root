24035a000d1500012040fc33012a36318b99dfa7
Bez Hermoso <bezalel@squareup.com>
2023-07-21 23:19:06 -0700

    Fixes
    First, don't use `exit`. That was bad. Second, it was cding w/ no
    argument if already on Git root and there wasn't any parent repo. That's
    now properly handled.

--- jump-to-git-root.sh
+++ jump-to-git-root.sh
@@ -1,12 +1,14 @@
-# Jump to Git repo's root
-# If already in root, check if in a sub-module. If so, go to nearest parent repo.
+#!/usr/bin/env zsh
+
+## Jump to Git repo's root
+# If already in root, check if in a sub-module. If so, jump to nearest parent repo.
 # Authors:
 #   Bez Hermoso (c) 2018
 function jump-to-git-root {
   local _root_dir="$(git rev-parse --show-toplevel 2>/dev/null)"
-  if [[ $? -gt 0 ]]; then
+  if [[ -z $_root_dir ]]; then
     >&2 echo 'Not a Git repo!'
-    exit 1
+    return 1
   fi
   local _pwd=$(pwd)
   if [[ $_pwd = $_root_dir ]]; then
@@ -14,7 +16,7 @@ function jump-to-git-root {
     # If parent dir is also managed under Git then we are in a submodule.
     # If so, cd to nearest Git parent project.
     _root_dir=$(git -C $(dirname $_pwd) rev-parse --show-toplevel 2>/dev/null)
-    if [[ $? -gt 0 ]]; then
+    if [[ -z $_root_dir ]]; then
       echo "Already at Git repo root."
       return 0
     fi
@@ -24,3 +26,5 @@ function jump-to-git-root {
   echo "Git repo root: $_root_dir"
   cd $_root_dir
 }
+
+alias git-jump-root='jump-to-git-root'
